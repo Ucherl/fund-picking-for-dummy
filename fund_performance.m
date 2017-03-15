4f2349a9ebfee34c735e3a20b4fa826e7309a271
Ucherl <hsnu126502@gmail.com>
2017-03-16 01:22:39 +0800

    Add files via upload
    - convert NAV, index to Return

--- fund_performance.m
+++ fund_performance.m
@@ -1,27 +1,37 @@
 function [alpha, beta] = fund_performance(d1,market,d2,fund,d3,risk_free,draw)
 %step1. rearrange the data, elimminate NAN and align d1=d2
 %step1.2 assume that d3==d1, if not, i will add one more loop then.
+%step1.3 convert NAV to return
 %step2. get alpha and beta by regression
 %step3. show figure or not
 %?????????column, ????????
 
 [m,~]=size(d2);
 [n,~]=size(d1);
-for_regression_metrix=[];
+for_regression=[];
 
 for i=1:m
     for j=1:n
         if (d2(i,1)==d1(j,1)) && isnan(fund(i,1))==0 && d2(i,1)==d3(j,1)
-            for_regression_metrix= [for_regression_metrix ; d2(i,1),fund(i,1),market(j,1),risk_free(j,1)];
+            for_regression= [for_regression ; d2(i,1),fund(i,1),market(j,1),risk_free(j,1)];
         end
     end
 end
 
+[k,~]=size(for_regression);
+
+new_for_regression=[];
+for g=1:k-1
+    new_for_regression=[new_for_regression; (for_regression(g+1,2)-for_regression(g,2))/for_regression(g,2), (for_regression(g+1,3)-for_regression(g,3))/for_regression(g,3),for_regression(g+1,4)];
+end
+
+
+
 %just for checking
 %disp(for_regression_metrix);
-y=for_regression_metrix(:,2)-for_regression_metrix(:,4);
+y=new_for_regression(:,1)-new_for_regression(:,3);
 %disp(y);
-x=for_regression_metrix(:,3)-for_regression_metrix(:,4);
+x=new_for_regression(:,2)-new_for_regression(:,3);
 %disp(x);
 
 p=polyfit(x,y,1);
